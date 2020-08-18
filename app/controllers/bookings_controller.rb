360d280fec52ae91f86d961a0f9caf2106cc20c0
Doerthe Kosicki <kiki@codestun.com>
2020-08-18 15:14:16 +0200

    Fixes for comments left in code

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,14 +9,11 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
-
-
   def create
     booking = Booking.new(booking_params)
     garden = Garden.find(params[:id])
     booking.garden = garden
     booking.save
-
     # if booking.save // else redirect_to 'bookings/new'_path(:booking)
   end
 
@@ -26,10 +23,3 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:start_date, :end_date)
   end
 end
-
-    @review = Review.new(review_params)
-    # we need `restaurant_id` to associate review with corresponding restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)

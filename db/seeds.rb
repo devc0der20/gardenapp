7ba24dd06cd984512b320f51a54f0e0fa26be6af
Georg Kloeck <georg@kloeck.com>
2020-08-18 14:52:09 +0200

    seed file is ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+  # user = User.create(email:Faker::Internet.email)
+10.times do
+  user = User.new
+  user.email = Faker::Internet.email
+  user.password = "1234567"
+  user.save!
+  p user
+end
+
+10.times do
+  garden = Garden.new
+  garden.price = rand(155)
+  garden.name = Faker::Address.city_prefix
+  garden.user_id = (1..10).to_a.sample
+  garden.description = Faker::Marketing.buzzwords
+  garden.address = Faker::Address.street_address
+  garden.save!
+  p garden
+end
+
+20.times do
+  booking = Booking.new
+  booking.start_date = Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
+  booking.end_date = Faker::Date.between(from: '2014-09-26', to: '2014-09-27')
+  booking.user_id = rand(1..10)
+  booking.garden_id = rand(1..10)
+  booking.save!
+  p booking
+end
+

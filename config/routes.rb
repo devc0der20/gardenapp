f3f82aa630b525b0422745f5c7de738d4ddbeec9
Doerthe Kosicki <kiki@codestun.com>
2020-08-19 14:29:32 +0200

    Add as: :garden so we can use the garden path helper in views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/gardens', to: "gardens#index"
-  get '/gardens/:id', to: "gardens#show"
+  get '/gardens/:id', to: "gardens#show", as: :garden
   get '/gardens/:id/bookings/new', to: "bookings#new"
   get '/bookings/:id', to: "bookings#show"
   post '/gardens/:id/bookings/', to: "bookings#create"

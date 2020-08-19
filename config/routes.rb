30fe0151d4e6656e69b5163bf07d8401027ebfeb
Georg Kloeck <georg@kloeck.com>
2020-08-19 15:25:50 +0200

    talked through with lead dev

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/gardens', to: "gardens#index"
-  get '/gardens/:id', to: "gardens#show"
-  get '/gardens/:id/bookings/new', to: "bookings#new", as: :booking_garden
+  get '/gardens/:id', to: "gardens#show", as: :garden
+  get '/gardens/:id/bookings/new', to: "bookings#new", as: :garden_bookings
   get '/bookings/:id', to: "bookings#show"
   post '/gardens/:id/bookings/', to: "bookings#create"
 end

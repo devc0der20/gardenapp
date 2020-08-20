b4d4ad47c1a64391d349b5218ab428c0694d4a99
Yusuf Ayub <yusayub@hotmail.com>
2020-08-20 15:24:52 +0200

    completed map, markers, adding info popup to the markers and search withautocomplete

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -3,11 +3,19 @@ class GardensController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @gardens = Garden.all
+    # @gardens = Garden.all
+    @gardens = Garden.geocoded
+    @markers = @gardens.map do |garden|
+      {
+        lat: garden.latitude,
+        lng: garden.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { garden: garden }),
+        # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+      }
+    end
   end
 
   def show
     @garden = Garden.find(params[:id])
   end
-
 end
